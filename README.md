24664b3391d809e9ad475a3f72d3f29057faa7fc
Elena <elena-kroener@web.de>
2023-02-07 16:43:40 +0100

    Add IDs

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # VocabularyNounReader
 
 Python script to read nouns and their gender from a pdf
+
+#### Run
+`python vocab.py -f PATH_TO_PDF_FILE`
--- vocab.py
+++ vocab.py
@@ -9,19 +9,23 @@ def read_pdf_to_json(file_path):
     all_pages = len(reader.pages)
     vocab = []
     genders = {"der": "m", "die": "f", "das": "n"}
+    index = 0
     for i in range(all_pages):
         page = reader.pages[i]
         text = page.extract_text()
         for row in text.split("\n"):
             row = row.split(" ")
             if row[0] in ["der", "die", "das"] and row[1][0].isupper():
-                vocab.append({"name": row[1], "gender": genders[row[0]]})
-
-    with open('vocab.json', 'w') as file:
+                vocab.append({"id": index, "name": row[1], "gender": genders[row[0]]})
+                index += 1
+    file_name = file_path.replace('.pdf', '') + '.json'
+    with open(file_name, 'w') as file:
         json.dump(vocab, file)
 
-def main():
-    read_pdf_to_json("a1_1.pdf")
+@click.command()
+@click.option('-f', '--file', help='Path to PDF.')
+def main(file):
+    read_pdf_to_json(file)
 
 if __name__ == '__main__':
     main()
