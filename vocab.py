724a18938aa94ce90b281021d21336ce47d2ecbc
Elena <elena-kroener@web.de>
2023-02-07 16:28:54 +0100

    Update format and read all pages

--- vocab.py
+++ vocab.py
@@ -1,18 +1,27 @@
+import click
 import json
 
 from PyPDF2 import PdfReader
 
-reader = PdfReader("a1_1.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
 
-vocab = {}
-genders = {"der": "m", "die": "f", "das": "n"}
-for row in text.split("\n"):
-    row = row.split(" ")
-    if row[0] in ["der", "die", "das"] and row[1][0].isupper():
-        vocab.update({row[1]: {"gender": genders[row[0]]}})
+def read_pdf_to_json(file_path):
+    reader = PdfReader(file_path)
+    all_pages = len(reader.pages)
+    vocab = []
+    genders = {"der": "m", "die": "f", "das": "n"}
+    for i in range(all_pages):
+        page = reader.pages[i]
+        text = page.extract_text()
+        for row in text.split("\n"):
+            row = row.split(" ")
+            if row[0] in ["der", "die", "das"] and row[1][0].isupper():
+                vocab.append({"name": row[1], "gender": genders[row[0]]})
 
-with open('vocab.json', 'w') as file:
-    json.dump(vocab, file)
+    with open('vocab.json', 'w') as file:
+        json.dump(vocab, file)
+
+def main():
+    read_pdf_to_json("a1_1.pdf")
+
+if __name__ == '__main__':
+    main()
